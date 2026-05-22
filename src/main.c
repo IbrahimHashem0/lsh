a904bb7127e50a3efa316e40a5630a636ad38047
Ibrahim Hashem <ebraheemhashem0@gmail.com>
2026-05-22 18:15:30 +0300

    Successfully implemented 4 custom builtin commands for OS project

--- src/main.c
+++ src/main.c
@@ -1,13 +1,7 @@
 /***************************************************************************//**
-
-  @file         main.c
-
-  @author       Stephen Brennan
-
-  @date         Thursday,  8 January 2015
-
-  @brief        LSH (Libstephen SHell)
-
+  @file        main.c
+ @author      Ibrahim Hashem (Modified Structure)
+  @brief       Customized LSH with new builtin commands for OS Project
 *******************************************************************************/
 
 #include <sys/wait.h>
@@ -17,6 +11,28 @@
 #include <stdio.h>
 #include <string.h>
 
+// --- Custom Environment and Command History tracking ---
+#define HISTORY_MAX_SIZE 150
+char *session_history[HISTORY_MAX_SIZE];
+int total_history_records = 0;
+
+extern char **environ;
+
+void log_command_history(char *input_line) {
+    if (input_line == NULL || strlen(input_line) == 0) return;
+    
+    if (total_history_records < HISTORY_MAX_SIZE) {
+        session_history[total_history_records] = strdup(input_line);
+        total_history_records++;
+    } else {
+        free(session_history[0]);
+        for (int idx = 1; idx < HISTORY_MAX_SIZE; idx++) {
+            session_history[idx - 1] = session_history[idx];
+        }
+        session_history[HISTORY_MAX_SIZE - 1] = strdup(input_line);
+    }
+}
+
 /*
   Function Declarations for builtin shell commands:
  */
@@ -24,19 +40,33 @@ int lsh_cd(char **args);
 int lsh_help(char **args);
 int lsh_exit(char **args);
 
+// --- My New Custom Builtin Prototypes ---
+int lsh_pwd(char **args);
+int lsh_echo(char **args);
+int lsh_history(char **args);
+int lsh_env(char **args);
+
 /*
   List of builtin commands, followed by their corresponding functions.
  */
 char *builtin_str[] = {
   "cd",
   "help",
-  "exit"
+  "exit",
+  "pwd",     
+  "echo",    
+  "history", 
+  "env"      
 };
 
 int (*builtin_func[]) (char **) = {
   &lsh_cd,
   &lsh_help,
-  &lsh_exit
+  &lsh_exit,
+  &lsh_pwd,     
+  &lsh_echo,    
+  &lsh_history, 
+  &lsh_env      
 };
 
 int lsh_num_builtins() {
@@ -49,8 +79,6 @@ int lsh_num_builtins() {
 
 /**
    @brief Builtin command: change directory.
-   @param args List of args.  args[0] is "cd".  args[1] is the directory.
-   @return Always returns 1, to continue executing.
  */
 int lsh_cd(char **args)
 {
@@ -66,13 +94,11 @@ int lsh_cd(char **args)
 
 /**
    @brief Builtin command: print help.
-   @param args List of args.  Not examined.
-   @return Always returns 1, to continue executing.
  */
 int lsh_help(char **args)
 {
   int i;
-  printf("Stephen Brennan's LSH\n");
+  printf("Custom LSH - Shell Project\n");
   printf("Type program names and arguments, and hit enter.\n");
   printf("The following are built in:\n");
 
@@ -86,18 +112,74 @@ int lsh_help(char **args)
 
 /**
    @brief Builtin command: exit.
-   @param args List of args.  Not examined.
-   @return Always returns 0, to terminate execution.
  */
 int lsh_exit(char **args)
 {
   return 0;
 }
 
+/*
+  Implementation of Builtin Command: pwd
+  Prints the current working directory path.
+*/
+int lsh_pwd(char **args) {
+    char current_path[2048];
+    if (getcwd(current_path, sizeof(current_path)) != NULL) {
+        printf("%s\n", current_path);
+    } else {
+        perror("lsh: error retrieving directory");
+    }
+    return 1;
+}
+
+/*
+  Implementation of Builtin Command: echo
+  Prints the arguments passed to the terminal screen.
+*/
+int lsh_echo(char **args) {
+    int index = 1;
+    for (; args[index] != NULL; index++) {
+        printf("%s", args[index]);
+        if (args[index + 1] != NULL) {
+            printf(" ");
+        }
+    }
+    printf("\n");
+    return 1;
+}
+
+/*
+  Implementation of Builtin Command: history
+  Displays the list of previously entered commands.
+*/
+int lsh_history(char **args) {
+    if (total_history_records == 0) {
+        printf("History cache is empty.\n");
+        return 1;
+    }
+    int counter = 0;
+    while (counter < total_history_records) {
+        printf(" [%d]  %s\n", counter + 1, session_history[counter]);
+        counter++;
+    }
+    return 1;
+}
+
+/*
+  Implementation of Builtin Command: env
+  Prints all environment variables in KEY=VALUE format.
+*/
+int lsh_env(char **args) {
+    char **current_env = environ;
+    while (*current_env != NULL) {
+        printf("%s\n", *current_env);
+        current_env++;
+    }
+    return 1;
+}
+
 /**
   @brief Launch a program and wait for it to terminate.
-  @param args Null terminated list of arguments (including program).
-  @return Always returns 1, to continue execution.
  */
 int lsh_launch(char **args)
 {
@@ -126,15 +208,12 @@ int lsh_launch(char **args)
 
 /**
    @brief Execute shell built-in or launch program.
-   @param args Null terminated list of arguments.
-   @return 1 if the shell should continue running, 0 if it should terminate
  */
 int lsh_execute(char **args)
 {
   int i;
 
   if (args[0] == NULL) {
-    // An empty command was entered.
     return 1;
   }
 
@@ -149,16 +228,15 @@ int lsh_execute(char **args)
 
 /**
    @brief Read a line of input from stdin.
-   @return The line from stdin.
  */
 char *lsh_read_line(void)
 {
 #ifdef LSH_USE_STD_GETLINE
   char *line = NULL;
-  ssize_t bufsize = 0; // have getline allocate a buffer for us
+  ssize_t bufsize = 0;
   if (getline(&line, &bufsize, stdin) == -1) {
     if (feof(stdin)) {
-      exit(EXIT_SUCCESS);  // We received an EOF
+      exit(EXIT_SUCCESS);
     } else  {
       perror("lsh: getline\n");
       exit(EXIT_FAILURE);
@@ -178,7 +256,6 @@ char *lsh_read_line(void)
   }
 
   while (1) {
-    // Read a character
     c = getchar();
 
     if (c == EOF) {
@@ -191,7 +268,6 @@ char *lsh_read_line(void)
     }
     position++;
 
-    // If we have exceeded the buffer, reallocate.
     if (position >= bufsize) {
       bufsize += LSH_RL_BUFSIZE;
       buffer = realloc(buffer, bufsize);
@@ -207,9 +283,7 @@ char *lsh_read_line(void)
 #define LSH_TOK_BUFSIZE 64
 #define LSH_TOK_DELIM " \t\r\n\a"
 /**
-   @brief Split a line into tokens (very naively).
-   @param line The line.
-   @return Null-terminated array of tokens.
+   @brief Split a line into tokens.
  */
 char **lsh_split_line(char *line)
 {
@@ -232,7 +306,7 @@ char **lsh_split_line(char *line)
       tokens_backup = tokens;
       tokens = realloc(tokens, bufsize * sizeof(char*));
       if (!tokens) {
-		free(tokens_backup);
+        free(tokens_backup);
         fprintf(stderr, "lsh: allocation error\n");
         exit(EXIT_FAILURE);
       }
@@ -256,6 +330,10 @@ void lsh_loop(void)
   do {
     printf("> ");
     line = lsh_read_line();
+    
+    // --- Call my custom log history function ---
+    log_command_history(line);
+
     args = lsh_split_line(line);
     status = lsh_execute(args);
 
@@ -266,19 +344,9 @@ void lsh_loop(void)
 
 /**
    @brief Main entry point.
-   @param argc Argument count.
-   @param argv Argument vector.
-   @return status code
  */
 int main(int argc, char **argv)
 {
-  // Load config files, if any.
-
-  // Run command loop.
   lsh_loop();
-
-  // Perform any shutdown/cleanup.
-
   return EXIT_SUCCESS;
 }
-
